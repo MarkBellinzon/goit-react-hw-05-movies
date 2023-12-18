4839a85c1f3c614da286135237156f4ee464ee75
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-12-18 18:21:24 +0200

    add_cast_hook

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ListItem, StyledList } from './Cast.module';
+import { getCastMovie } from 'helpers/getMovies';
+
+const Cast = () => {
+  const { movieId } = useParams();
+
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    const fetchCast = async () => {
+      try {
+        const cast = await getCastMovie(movieId);
+        setCast(cast);
+      } catch (e) {}
+    };
+    fetchCast();
+  }, [movieId]);
+
+  return (
+    <>
+      {
+        <StyledList>
+          {cast.map(({ id, profile_path, original_name, character }) => (
+            <ListItem key={id}>
+              <p>
+                <span> Actor:</span> {original_name}
+              </p>
+              <p>
+                <span>Character:</span> {character}
+              </p>
+            </ListItem>
+          ))}
+        </StyledList>
+      }
+    </>
+  );
+};
+
+export default Cast;
